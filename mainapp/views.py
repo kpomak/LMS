7f0d4240a24acf6ff5700288e3ad744caa701890
Roman Ryzhkov <93090129+kpomak@users.noreply.github.com>
2022-09-28 18:37:12 +0300

    templates
    * template including
    
    * mailto filter
    
    * one more time
    
    * simple - dimle
    
    * working links
    
    * end today
    
    * Вывод в цикле новостей из JSON
    
    * сортировка импортов
    
    * Перенаправление на поисковик с передачей параметров
    
    * подумываю удалить isort

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,9 @@
+import json
+import os
+from datetime import datetime
+
+from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView
 
 
@@ -7,6 +13,25 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = "mainapp/news.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        with open(os.path.join(settings.BASE_DIR, "news.json")) as f:
+            context["news"] = json.load(f)
+
+        context["news_title"] = "Горящий заголовок"
+        context["news_preview"] = "Захватывающее описание"
+        context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
+        return context
+
+
+class NewsWithPaginatorView(NewsPageView):
+    def get_context_data(self, page, **kwargs):
+        context = super().get_context_data(page=page, **kwargs)
+        context["page_num"] = page
+        return context
 
 
 class CoursesPageView(TemplateView):
@@ -23,3 +48,7 @@ class DocSitePageView(TemplateView):
 
 class LoginPageView(TemplateView):
     template_name = "mainapp/login.html"
+
+
+def search_redirect(request):
+    return HttpResponseRedirect("https://www.google.com/search?q=" + request.GET["q"])
