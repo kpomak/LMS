27c615b05ddee903799ce6c9b49694c79f6eabbb
kpomak <romka_ne@mail.ru>
2022-09-28 05:03:38 +0300

    mailto filter

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-
+from datetime import datetime
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
@@ -14,6 +14,8 @@ class NewsPageView(TemplateView):
         # Create your own data
         context["news_title"] = "Громкий новостной заголовок"
         context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        context["range"] = range(5)
+        context["datetime_obj"] = datetime.now()
         return context
 
 
