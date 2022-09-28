e86e148b31aee953902a436b633ab0992a950937
kpomak <romka_ne@mail.ru>
2022-09-28 05:07:36 +0300

    one more time

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,5 +1,7 @@
-from django.views.generic import TemplateView
 from datetime import datetime
+
+from django.views.generic import TemplateView
+
 
 class MainPageView(TemplateView):
     template_name = "mainapp/index.html"
