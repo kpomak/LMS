6be7d40a41ced21f970f4c6f2a470d09d2611496
kpomak <romka_ne@mail.ru>
2022-10-06 20:35:07 +0300

    merge lesson-6

--- authapp/admin.py
+++ authapp/admin.py
@@ -1,1 +1,11 @@
-# Register your models here.
+from django.contrib import admin
+
+from authapp import models as authapp_models
+
+
+@admin.register(authapp_models.CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email", "is_active", "date_joined"]
+    list_editable = ["is_active"]
+    ordering = ["-date_joined"]
+    list_filter = ["is_active", "is_staff"]
--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,1 +1,71 @@
-# Register your models here.
+from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
+
+from mainapp import models as mainapp_models
+
+
+class InputFilter(admin.SimpleListFilter):
+    template = "admin/input_filter.html"
+
+    def lookups(self, request, model_admin):
+        return ((),)
+
+    def choices(self, changelist):
+        all_choice = next(super().choices(changelist))
+        all_choice["query_parts"] = (
+            (k, v) for k, v in changelist.get_filters_params().items() if k != self.parameter_name
+        )
+        yield all_choice
+
+
+class TextInputFilter(InputFilter):
+    parameter_name = "text"
+    title = _("text in news title")
+
+    def queryset(self, request, queryset):
+        if self.value() is not None:
+            text = self.value()
+
+            return queryset.filter(title__contains=text)
+
+
+@admin.register(mainapp_models.News)
+class NewsAdmin(admin.ModelAdmin):
+    search_fields = ["title", "preambule", "body"]
+    list_filter = [TextInputFilter, "created"]
+    date_hierarchy = "created"
+
+
+@admin.register(mainapp_models.Lesson)
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ["id", "get_course_name", "num", "title", "deleted"]
+    ordering = ["-course__name", "-num"]
+    list_per_page = 5
+    list_filter = ["course", "created", "deleted"]
+    actions = ["mark_deleted", "mark_undeleted"]
+
+    def get_course_name(self, obj):
+        return obj.course.name
+
+    get_course_name.short_description = _("Cou5rse")
+
+    def mark_deleted(self, request, queryset):
+        queryset.update(deleted=True)
+
+    mark_deleted.short_description = _("Mark deleted")
+
+    def mark_undeleted(self, request, queryset):
+        queryset.update(deleted=False)
+
+    mark_undeleted.short_description = _("Mark undeleted")
+
+
+@admin.register(mainapp_models.CourseTeachers)
+class CourseTeachersAdmin(admin.ModelAdmin):
+    list_display = ["id", "__str__", "get_courses"]
+    list_select_related = True
+
+    def get_courses(self, obj):
+        return ", ".join((i.name for i in obj.course.all()))
+
+    get_courses.short_description = _("Courses")
